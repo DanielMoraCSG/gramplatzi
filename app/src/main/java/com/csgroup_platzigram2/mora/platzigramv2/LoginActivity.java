d39613beacd7bbefbd808f176fbab771b326d628
DanielMoraCSG <daniel@csgroup.mx>
2016-10-18 16:07:59 -0500

    Actualizacion de leaks frames con estilo Android

--- app/src/main/java/com/csgroup_platzigram2/mora/platzigramv2/LoginActivity.java
+++ app/src/main/java/com/csgroup_platzigram2/mora/platzigramv2/LoginActivity.java
@@ -3,8 +3,10 @@ package com.csgroup_platzigram2.mora.platzigramv2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.csgroup_platzigram2.mora.platzigramv2.view.ContainerActivity;
 import com.csgroup_platzigram2.mora.platzigramv2.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,6 +19,12 @@ public class LoginActivity extends AppCompatActivity {
 
     public void goCreateAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void goHome(View view){
+        Log.e("Mensaje","Aquí valio madres");
+        Intent intent = new Intent(this, ContainerActivity.class);
         startActivity(intent);
     }
 }
